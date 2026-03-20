2d3fa748ad1cf74771dd98911686a7120481823c
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2026-03-20 15:47:20 +0530

    added port to run smoothly

--- app.py
+++ app.py
@@ -1231,4 +1231,5 @@ def seed_admin_if_missing():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
